c588c05daff63cee7f1c1c5d373ae9edbeb1d848
root <root@DESKTOP-AE1SKMA.>
2024-10-15 17:23:47 +0200

    Test

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: ttroll <ttroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 11:15:02 by ttroll            #+#    #+#             */
-/*   Updated: 2024/10/15 17:17:06 by ttroll           ###   ########.fr       */
+/*   Updated: 2024/10/15 17:23:07 by ttroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ void *my_memset(void *ptr, int value, size_t num) {
 	
     unsigned char *p = (unsigned char *)ptr;
 
- 
+Test
     // "value" wird ebenfalls in ein Byte (unsigned char) umgewandelt.
     
     // Als nächstes solltest du eine Schleife machen, um den Speicher zu füllen.
